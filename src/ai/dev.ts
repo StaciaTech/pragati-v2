35af22538d62d3404ffa65599790caae873dc50c
varma1201 <vikramvarma1201@gmail.com>
2025-09-08 11:58:28 +0530

    change log -cleared old code

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-validation-report.ts';
-import '@/ai/flows/summarize-validation-data.ts';
-import '@/ai/flows/generate-html-report.ts';
-import '@/ai/flows/generate-evaluation.ts';
